8b87f2b4486e3fee0b4d3c40f6b18a48fadfc446
kuflievskiy <kuflievskiy@gmail.com>
2016-10-08 21:48:20 +0300

    small fixes

--- app/app.routing.ts
+++ app/app.routing.ts
@@ -1,12 +1,13 @@
 import { Routes, RouterModule }   from '@angular/router';
 import {ModuleWithProviders} from "@angular/core";
-
+import {ContactComponent} from './contact.component';
 
 import {AlbumsComponent} from './albums.component';
 
 const appRoutes: Routes = [
     { path: '', component: AlbumsComponent }
-   
+    ,{ path: 'albums', component: AlbumsComponent }
+    ,{ path: 'contact',component: ContactComponent }
 ];
 
 export const appRoutingProviders: any[] = [
--- app/contact.component.ts
+++ app/contact.component.ts
@@ -1,9 +1,24 @@
 import {Component} from '@angular/core';
+import {FormGroup, FormBuilder, Validators} from "@angular/forms";
 
 @Component({
     templateUrl: '/app/contact.component.html'
 })
 export class ContactComponent {
+
+    form: FormGroup;
+
+    constructor(private fb: FormBuilder){
+        this.form = this.fb.group({
+            name: ['', [
+                Validators.required
+                ]
+            ],
+            comment: ['', [
+                Validators.required
+            ]]
+        });
+    }
     onSubmit(form){
         console.log(form);
     }
