7c65efc16a0c013cef6c3a15fc5529a96a7a8ca7
kuflievskiy <kuflievskiy@gmail.com>
2016-10-09 23:01:49 +0300

    CanDeactivateContactComponent

--- app/app.routing.ts
+++ app/app.routing.ts
@@ -5,16 +5,34 @@ import {ContactComponent} from './contact.component';
 import {AlbumsComponent} from './albums.component';
 import {AlbumComponent} from './album.component';
 
+
+///
+import {Injectable} from "@angular/core";
+import { ActivatedRouteSnapshot, RouterStateSnapshot, CanDeactivate }   from '@angular/router';
+
+@Injectable()
+class CanDeactivateContactComponent implements CanDeactivate<ContactComponent> {
+    constructor() {}
+    canDeactivate(
+        component: ContactComponent,
+        route: ActivatedRouteSnapshot,
+        state: RouterStateSnapshot
+    ) {
+        return confirm('Are you sure?');
+    }
+}
+////
+
 const appRoutes: Routes = [
     { path: '', component: AlbumsComponent }
     ,{ path: 'albums', component: AlbumsComponent }
     ,{ path: 'album/:id', component: AlbumComponent }
-    ,{ path: 'contact',component: ContactComponent }
+    ,{ path: 'contact',component: ContactComponent,  canDeactivate: [CanDeactivateContactComponent] }
     , { path : '**', redirectTo: 'albums'}
 ];
 
 export const appRoutingProviders: any[] = [
-
+    CanDeactivateContactComponent
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
